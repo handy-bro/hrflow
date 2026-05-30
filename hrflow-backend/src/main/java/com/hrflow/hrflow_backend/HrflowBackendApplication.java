9a6973bbd42e2afcbc1aa9cee768b05289229a31
handy-bro <handymelong237@gmail.com>
2026-05-30 04:20:15 +0100

    chore: initial project structure with backend and frontend

---
+++ hrflow-backend/src/main/java/com/hrflow/hrflow_backend/HrflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hrflow.hrflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrflowBackendApplication.class, args);
+	}
+
+}
